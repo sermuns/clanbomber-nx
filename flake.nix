011222d2c73de70794495e99e94e14b2aa00afe0
Samuel Åkesson <sermuns@lysator.liu.se>
2026-05-03 17:51:35 +0200

    format and add boost libs and shit

--- flake.nix
+++ flake.nix
@@ -5,38 +5,65 @@
     devkitNix.url = "github:bandithedoge/devkitNix";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    devkitNix,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        # devkitNix provides an overlay with the toolchains
-        overlays = [devkitNix.overlays.default];
-      };
-    in {
-      # devkitNix provides standard environments which set all the necessary variables and add dependencies.
-      # No more shell hooks!
-      devShells.default = pkgs.mkShell.override {stdenv = pkgs.devkitNix.stdenvA64;} {};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      devkitNix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          # devkitNix provides an overlay with the toolchains
+          overlays = [ devkitNix.overlays.default ];
+        };
+      in
+      {
+        # devkitNix provides standard environments which set all the necessary variables and add dependencies.
+        # No more shell hooks!
+        devShells.default =
+          pkgs.mkShell.override
+            {
+              stdenv = pkgs.devkitNix.stdenvA64;
+            }
+            {
+              buildInputs = [
+                pkgs.boost
+                pkgs.boost.dev
+                pkgs.SDL
+                pkgs.SDL_image
+                pkgs.SDL_mixer
+                pkgs.SDL_ttf
+                pkgs.SDL_gfx
+                pkgs.pkg-config
+              ];
 
-      packages.default = pkgs.stdenv.mkDerivation {
-        name = "devkitA64-example";
-        src = ./.;
+              shellHook = ''
+                export BOOST_ROOT=${pkgs.boost.dev}/include/boost
+                export PKG_CONFIG_PATH=${pkgs.SDL}/lib/pkgconfig:$PKG_CONFIG_PATH
+                export LDFLAGS="-L${pkgs.SDL_image}/lib -L${pkgs.SDL_mixer}/lib -L${pkgs.SDL_ttf}/lib -L${pkgs.SDL_gfx}/lib $LDFLAGS"
+              '';
+            };
 
-        # The `TARGET` variable is used by devkitPro's example Makefiles to set the name of the executable.
-        makeFlags = ["TARGET=example"];
-        # This is a simple Switch app example that only builds a single
-        # executable. If your project outputs multiple files, make `$out` a
-        # directory and copy everything there.
-        installPhase = ''
-          mkdir $out
-          cp example.nro $out
-        '';
-      };
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "devkitA64-example";
+          src = ./.;
 
-    });
+          # The `TARGET` variable is used by devkitPro's example Makefiles to set the name of the executable.
+          makeFlags = [ "TARGET=example" ];
+          # This is a simple Switch app example that only builds a single
+          # executable. If your project outputs multiple files, make `$out` a
+          # directory and copy everything there.
+          installPhase = ''
+            mkdir $out
+            cp example.nro $out
+          '';
+        };
+
+      }
+    );
 }
